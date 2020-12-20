e73d8ee6a689024dcd3b4ef021ebcaff03c93e9a
pemburu-kage <angkaterbaik9@gmail.com>
2020-12-20 12:14:44 +0800

    Menampilkan Product

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. First Commit : Create react app, install react-bootstrap, sweetalert2, axios
 2. Setting Tema, Font, dan Layout
+3. Menampilkan Product
--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
+import React, { Component } from 'react';
 import { Row, Col, Container } from "react-bootstrap";
-import {Hasil, ListCategories, NavbarComponent } from "./components";
+import {Hasil, ListCategories, Menus, NavbarComponent } from "./components";
+import { API_URL } from './utils/constants'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
-      </div>
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       menus: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL+"products")
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log("Error yaa " , error);
+      })
+  }
+
+  render() {
+    const { menus } = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+              </Col>
+              <Row>
+                {menus && menus.map((menu) => (
+                  <Menus 
+                    key={menu.id}
+                    menu={menu}
+                  />
+                ))}
+              </Row>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
