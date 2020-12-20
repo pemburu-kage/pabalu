6dcf9610b2d021d84cdfd99901532796f3c30cac
pemburu-kage <angkaterbaik9@gmail.com>
2020-12-20 14:22:29 +0800

    Menampilkan Produk berdasarkan Category

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. First Commit : Create react app, install react-bootstrap, sweetalert2, axios
 2. Setting Tema, Font, dan Layout
 3. Menampilkan Product
+4. Menampilkan Produk berdasarkan Category
--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import {Hasil, ListCategories, Menus, NavbarComponent } from "./components";
 import { API_URL } from './utils/constants'
 import axios from 'axios'
 
-
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
        menus: [],
+       categoriYangDipilih: 'Makanan'
     }
   }
 
   componentDidMount() {
     axios
-      .get(API_URL+"products")
+      .get(API_URL+"products?category.nama="+this.state.categoriYangDipilih)
       .then(res => {
         const menus = res.data;
         this.setState({ menus });
@@ -26,29 +26,47 @@ export default class App extends Component {
       })
   }
 
+  changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: []
+    })
+
+    axios
+      .get(API_URL+"products?category.nama="+value)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log("Error yaa " , error);
+      })
+
+  }
+
   render() {
-    const { menus } = this.state
+    const { menus, categoriYangDipilih } = this.state
     return (
       <div className="App">
         <NavbarComponent />
         <div className="mt-3">
           <Container fluid>
             <Row>
-              <ListCategories />
+            <ListCategories changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih}/>
               <Col>
                 <h4>
                   <strong>Daftar Produk</strong>
                 </h4>
                 <hr />
+                <Row>
+                  {menus && menus.map((menu) => (
+                    <Menus 
+                      key={menu.id}
+                      menu={menu}
+                    />
+                  ))}
+                </Row>
               </Col>
-              <Row>
-                {menus && menus.map((menu) => (
-                  <Menus 
-                    key={menu.id}
-                    menu={menu}
-                  />
-                ))}
-              </Row>
               <Hasil />
             </Row>
           </Container>
